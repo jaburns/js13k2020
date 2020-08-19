a34badf65321e6031867245a8088bf2753099eee
jaburns <j@jaburns.net>
2020-08-19 13:52:46 -0600

    Indentation fix

--- build.js
+++ build.js
@@ -14,54 +14,54 @@ const run = cmd =>
 
 const buildBundle = () =>
 {
-	run( 'tsc --outDir build' );
-	run( 'rollup -c' );
+    run( 'tsc --outDir build' );
+    run( 'rollup -c' );
 };
 
 const regpacked = js =>
 {
-	return new ShapeShifter().preprocess(js, {
-		hashWebGLContext: true,
-		contextVariableName: 'g',
-		contextType: 1,
-		reassignVars: true,
-		varsNotReassigned: ['g','a'],
-		useES6: true,
-	})[2].contents;
+    return new ShapeShifter().preprocess(js, {
+        hashWebGLContext: true,
+        contextVariableName: 'g',
+        contextType: 1,
+        reassignVars: true,
+        varsNotReassigned: ['g','a'],
+        useES6: true,
+    })[2].contents;
 };
 
 const fixDoubleQuotes = js =>
-	js.replace(/"/g, "'");
+    js.replace(/"/g, "'");
 
 const wrapWithHTML = js =>
 {
-	const htmlTemplate = fs.readFileSync('src/index.html', 'utf8')
-		.split('\n')
-		.map( line => line.trim() )
-		.join('');
+    const htmlTemplate = fs.readFileSync('src/index.html', 'utf8')
+        .split('\n')
+        .map( line => line.trim() )
+        .join('');
 
-	return htmlTemplate.replace('__CODE__', js);
+    return htmlTemplate.replace('__CODE__', js);
 };
 
 const main = () =>
 {
-	buildBundle();
+    buildBundle();
 
-	let x = fs.readFileSync('build/bundle.js', 'utf8');
-	x = regpacked( x );
-	x = fixDoubleQuotes( x );
-	x = wrapWithHTML( x );
+    let x = fs.readFileSync('build/bundle.js', 'utf8');
+    x = regpacked( x );
+    x = fixDoubleQuotes( x );
+    x = wrapWithHTML( x );
 
-	fs.writeFileSync( 'build/out.html', x );
+    fs.writeFileSync( 'build/out.html', x );
 
-	run( 'tools/advzip/advzip-linux-x64 --shrink-insane -i 10 -a out.zip build/out.html' );
+    run( 'tools/advzip/advzip-linux-x64 --shrink-insane -i 10 -a out.zip build/out.html' );
 
-	const zipStat = fs.statSync('out.zip');
-	const percent = Math.floor((zipStat.size / 13312) * 100) / 100;
+    const zipStat = fs.statSync('out.zip');
+    const percent = Math.floor((zipStat.size / 13312) * 100) / 100;
 
-	console.log(''); 
-	console.log(`  Final bundle size: ${zipStat.size} / 13312 bytes (${percent} %)`);
-	console.log(''); 
+    console.log(''); 
+    console.log(`  Final bundle size: ${zipStat.size} / 13312 bytes (${percent} %)`);
+    console.log(''); 
 };
 
 main();
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ declare const g: WebGLRenderingContext;
 
 export const boot = () =>
 {
-	g.clearColor(0,1,0,1);
-	g.clear(g.COLOR_BUFFER_BIT);
+    g.clearColor(0,1,0,1);
+    g.clear(g.COLOR_BUFFER_BIT);
 
-	let prog = g.createProgram()!;
-	let shader = g.createShader(g.VERTEX_SHADER)!;
-	g.shaderSource(shader, '');
-	g.compileShader(shader);
-	g.attachShader(prog, shader);
+    let prog = g.createProgram()!;
+    let shader = g.createShader(g.VERTEX_SHADER)!;
+    g.shaderSource(shader, '');
+    g.compileShader(shader);
+    g.attachShader(prog, shader);
 };
