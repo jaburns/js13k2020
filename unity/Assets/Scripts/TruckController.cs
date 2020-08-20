bb53baaa66b91be8414dc8ce3b9e6714f920f6c2
jaburns <j@jaburns.net>
2020-08-19 22:36:53 -0600

    Added prototyping unity project

---
+++ unity/Assets/Scripts/TruckController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckController : MonoBehaviour
+{
+    Vector3 oldPosition;
+
+    Vector3 velocity;
+    Vector3 position;
+
+    float subtickTime;
+
+    void Awake()
+    {
+        position = transform.position;
+        oldPosition = position;
+    }
+
+    void FixedUpdate()
+    {
+        subtickTime = 0;
+        oldPosition = position;
+
+        velocity += Physics.gravity * Time.fixedDeltaTime;
+        position += velocity * Time.fixedDeltaTime;
+    }
+
+    void Update()
+    {
+        subtickTime += Time.deltaTime;
+        var t = subtickTime / Time.fixedDeltaTime;
+
+        transform.position = oldPosition + t * (position - oldPosition);
+    }
+}
